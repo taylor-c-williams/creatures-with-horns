864e040de636922a02b3f9ba92375ccfeb78976d
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-09-22 15:55:46 -0700

    refactored dropdown

--- src/ImageList.js
+++ src/ImageList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ImageItem from './ImageItem.js'
 import Creatures from './data.js'
-
+import Dropdown from './Dropdown'
 export default class ImageList extends Component {
     state = {
         keyword: '',
@@ -10,6 +10,7 @@ export default class ImageList extends Component {
     
     handleChange = (e) => {
     this.setState({ keyword: e.target.value })
+    
     }
 
     handleHorns = (e) => {
@@ -25,29 +26,9 @@ export default class ImageList extends Component {
 
         return (
             <div>
-                  <p>Show all {this.state.keyword} Creatures</p>
-                <select onChange={this.handleChange} >
-                    <option value="">All Creatures</option>
-                    <option value="narwhal">Narwhal</option>
-                    <option value="rhino">Rhino</option>
-                    <option value="unicorn">Unicorn</option>
-                    <option value="unilego">Unilego</option>
-                    <option value="triceratops">Triceratops</option>
-                    <option value="Markhor">Markhor</option>
-                    <option value="mouflon">Mouflon</option>
-                    <option value="addax">Addax</option>
-                    <option value="chameleon">Chameleon</option>
-                    <option value="lizard">Lizard</option>
-                    <option value="dragon">Dragon</option>
-                </select>
-                Number of horns
-                <select onChange={this.handleHorns} >
-                    <option value="">All Creatures</option>
-                    <option value= {Number(1)}>1</option>
-                    <option value={Number(2)}>2</option>
-                    <option value={Number(3)}>3</option>
-                    <option value={Number(100)}>100</option>
-                </select>
+                 <Dropdown handleChange = {this.handleChange}
+                 handleHorns = {this.handleHorns} />
+
 
                 {FilteredCreatures.map(creature =>(<ImageItem
                 url = {creature.url} 
